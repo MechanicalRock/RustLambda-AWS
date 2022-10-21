a327b51364e0be053efa82ddcd4fbbd7cd93c8e2
Leon Ticharwa <leon.ticharwa@mechanicalrock.io>
2022-10-21 12:11:33 +0800

    lambda function name

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 aws lambda invoke \
 --cli-binary-format raw-in-base64-out \
 --region ap-southeast-2 \
---function-name HelloFriend-HelloFriendFunction-S5aicZwDLQsw \
+--function-name <your_lambda_function_name> \
 --payload '{"first_name": "Leon", "last_name":"Ticharwa"}' \
 output.json
